f48a759f9c0ad765b31748918746d996576995cf
Raghav Ramesh <raghavramesh94@gmail.com>
2016-09-02 18:10:59 +0800

    Processed raw bidding info data into round, open bidding period, closed bidding period

--- scraper.js
+++ scraper.js
@@ -5,5 +5,15 @@ req('http://www.nus.edu.sg/cors/schedule.html', function(err, body) {
   if (err) { return; }
 
   var $ = cheerio.load(body);
-  console.log($('table:nth-child(5)').text());
+  var timetable = [];
+  var roundInfo = {};
+
+  for (var i = 2; i < 10; i++) {
+    roundInfo.round = $('table:nth-child(5) tr:nth-child('+i+') td:nth-child(1)').text();
+    roundInfo.openBiddingPeriod = $('table:nth-child(5) tr:nth-child('+i+') td:nth-child(2)').text();
+    roundInfo.closedBiddingPeriod = $('table:nth-child(5) tr:nth-child('+i+') td:nth-child(3)').text();
+    timetable.push(roundInfo);
+  }
+
+  console.log(timetable);
 });
