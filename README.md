17f4d2b2b5325127fb673d764f09c1afdbb752ad
Raghav Ramesh <raghavramesh94@gmail.com>
2016-10-15 10:02:25 +0800

    Added README. Fixed algo.

---
+++ README.md
@@ -0,0 +1,5 @@
+NUS CORS Schedule Scraper
+=========================
+
+### What is it?
+A simple script that scrapes the module bidding timetable of NUS. It is supposed to be a small addition to an open source project.
--- scraper.js
+++ scraper.js
@@ -1,14 +1,17 @@
-var cheerio = require('cheerio');
-var req = require('tinyreq');
+'use strict';
+
+const cheerio = require('cheerio');
+const req = require('tinyreq');
 
 req('http://www.nus.edu.sg/cors/schedule.html', function(err, body) {
   if (err) { return; }
 
-  var $ = cheerio.load(body);
-  var timetable = [];
-  var roundInfo = {};
+  let $ = cheerio.load(body);
+  let timetable = [];
+  let roundInfo = {};
 
-  for (var i = 2; i < 10; i++) {
+  for (let i = 2; i < 10; i++) {
+    roundInfo = {};
     roundInfo.round = $('table:nth-child(5) tr:nth-child('+i+') td:nth-child(1)').text();
     roundInfo.openBiddingPeriod = $('table:nth-child(5) tr:nth-child('+i+') td:nth-child(2)').text();
     roundInfo.closedBiddingPeriod = $('table:nth-child(5) tr:nth-child('+i+') td:nth-child(3)').text();
